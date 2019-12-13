bb72750d0d2442284e8286d355b68d5292d7a75e
Aliona Shmeyerava <alionashm@yandex.by>
2019-12-13 16:34:59 +0300

    Update

--- src/main/java/app/Hotel/HouseCategory.java
+++ src/main/java/app/Hotel/HouseCategory.java
@@ -4,16 +4,10 @@ package app.Hotel;
  * The enum Truck category.
  */
 public enum HouseCategory {
-    /**
-     * Heavy truck category.
-     */
-    HEAVY,
-    /**
-     * Medium truck category.
-     */
-    MEDIUM,
-    /**
-     * Light truck category.
-     */
-    LIGHT
+
+    FAMILY,
+
+    COUPLE,
+
+    SINGLE
 }
